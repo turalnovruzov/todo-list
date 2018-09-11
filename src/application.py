a2e2a46f75f6e16699e50283a5ddd2f10090447a
Tural Novruzov <tural.nt.1@gmail.com>
2018-09-11 12:46:19 +0400

    Added layout and login views. /login route added for GET.

--- src/application.py
+++ src/application.py
@@ -9,5 +9,29 @@ app = Flask(__name__)
 # Configure the app
 app.config.from_object(config.Config())
 
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
 # Initialize Session
 Session(app)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    """Log user in"""
+    # If request method is POST
+    if request.method == 'POST':
+        # Log the user in
+        pass
+
+    # If request method is GET
+    else:
+        # Render the login.html
+        return render_template('login.html')
