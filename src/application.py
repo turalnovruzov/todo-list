34f7cb44f8f5f26ded746a0a876c01eec8cc14a2
Tural Novruzov <tural.nt.1@gmail.com>
2018-09-14 12:20:36 +0400

    Implemented signup, login, logout, and message flashing.

--- src/application.py
+++ src/application.py
@@ -1,6 +1,6 @@
 import config
-from flask import Flask, render_template, redirect, request
-from flask_login import UserMixin
+from flask import Flask, render_template, redirect, request, make_response, flash, jsonify
+from flask_login import UserMixin, LoginManager, login_user, login_required, logout_user, current_user
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import check_password_hash, generate_password_hash
 
@@ -10,8 +10,12 @@ app = Flask(__name__)
 # Configure the app
 app.config.from_object(config.Config())
 
-# Initialize app for db
+# Initialize database
 db = SQLAlchemy(app)
+
+# Initialize LoginManager
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 
 # Models
@@ -19,6 +23,10 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.Text, unique=True, nullable=False)
     password = db.Column(db.Text, nullable=False)
+
+    def __init__(self, username=None, password=None):
+        self.username = username
+        self.password = password
 
     def __repr__(self):
         return f'User({self.id}, {self.username})'
@@ -35,6 +43,12 @@ class Todo(db.Model):
         return f'Todo({self.id}, {self.content}, {self.user_id})'
 
 
+# User loader for login manager
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
 # Ensure responses aren't cached
 @app.after_request
 def after_request(response):
@@ -47,10 +61,38 @@ def after_request(response):
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     """Log user in"""
+
+    # Check if there is a logged in user
+    if current_user.is_authenticated:
+        return redirect('/')
+
     # If request method is POST
     if request.method == 'POST':
+
+        # Ensure username is provided
+        if not request.form.get('username'):
+            return make_response(('Username required', 400))
+
+        # Ensure password is provided
+        if not request.form.get('password'):
+            return make_response(('Password required', 400))
+
+        # Query database for user with the username
+        user = User.query.filter_by(username=request.form.get('username')).first()
+
+        # Check username exists and passwords match, if not flash error message and redirect to /login
+        if not user or not check_password_hash(user.password, request.form.get('password')):
+            flash('Username or password wrong', 'danger')
+            return redirect('/login')
+
+        # Get remember me
+        remember = request.form.get('remember') if request.form.get('remember') else False
+
         # Log the user in
-        pass
+        login_user(user, remember)
+
+        # Redirect to /
+        return redirect('/')
 
     # If request method is GET
     else:
@@ -61,12 +103,90 @@ def login():
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     """Sign user up"""
+
+    # Check if there is a logged in user
+    if current_user.is_authenticated:
+        return redirect('/')
+
     # If request method is POST
     if request.method == 'POST':
-        # Sign the user up
-        pass
+        # Ensure username exists
+        if not request.form.get('username'):
+            return make_response(('Username required', 400))
+
+        # Ensure password exists
+        if not request.form.get('password'):
+            return make_response(('Password required', 400))
+
+        # Ensure confirmation exists
+        if not request.form.get('confirmation'):
+            return make_response(('Confirmation required', 400))
+
+        # Check if passwords match
+        if request.form.get('password') != request.form.get('confirmation'):
+            return make_response(('Passwords don\'t match', 400))
+
+        # Check if username is already taken
+        if User.query.filter_by(username=request.form.get('username')).first():
+            return make_response(('Username is already taken.', 400))
+
+        # Create the User
+        user = User(request.form.get('username'),
+                    generate_password_hash(request.form.get('password')))
+
+        # Insert the user to database
+        db.session.add(user)
+        db.session.commit()
+
+        # Get remember me value
+        remember = request.form.get('remember') if request.form.get('remember') else False
+
+        # Log the user in
+        login_user(user, remember)
+
+        # Flash message
+        flash('Signed you up!', 'success')
+
+        # Redirect to /
+        return redirect('/signup')
 
     # If request method is GET
     else:
         # Render signup.html
         return render_template('signup.html')
+
+
+@app.route('/username_check', methods=['POST'])
+def username_check():
+    """
+    Checks if username is already taken
+
+    Response:
+    Json data containing username: string, and exists: boolean
+    """
+
+    # Ensure username is provided
+    if not request.form.get('username'):
+        return make_response(('Username required', 400))
+
+    # Create the result object
+    result = {
+        'username': request.form.get('username')
+    }
+
+    # Check database for username
+    result['exists'] = True if User.query.filter_by(username=result['username']).first() else False
+
+    # Return json
+    return jsonify(result)
+
+
+@app.route('/logout')
+def logout():
+    """Logs the user out"""
+
+    # Log the user out
+    logout_user()
+
+    # Redirect to login page
+    return redirect('/login')
---
+++ src/static/script.js
@@ -0,0 +1,52 @@
+$(document).ready(function () {
+    $.validator.addMethod("usernameCheck", function(value, element){
+        let return_value = true;
+        $.ajax({
+            type: "POST",
+            url: "/username_check",
+            data: { username: value },
+            success: (data) => {
+                return_value = !data.exists;
+            },
+            async: false
+          });
+        console.log(return_value);
+        return return_value;
+    }, "Username is already taken."); 
+
+    $("#signup-form").validate({
+        rules: {
+            username: {
+                required: true,
+                usernameCheck: true
+            },
+            password: {
+                required: true,
+                minlength: 5
+            },
+            confirmation: {
+                required: true,
+                equalTo: "#password"
+            }
+        },
+        messages: {
+            password: {
+                minlength: "Password must be at least 5 characters long."
+            },
+            confirmation: {
+                equalTo: "Passwords don't match."
+            }
+        },
+        errorElement: "div",
+        errorPlacement: (error, element) => {
+            $(error).addClass("invalid-feedback");
+            $(error).insertAfter(element);
+        },
+        highlight: (element, errorClass, validClass) => {
+            $(element).addClass("is-invalid").removeClass("is-valid");
+        },
+        unhighlight: (element, errorClass, validClass) => {
+            $(element).addClass("is-valid").removeClass("is-invalid");
+        }
+    });
+});
